25b974615446a044bbd3e580af4c77b88557f4b8
wang qianyi <wangqianyi@wangs-MacBook-Air.local>
2016-11-22 00:58:43 -0500

    add clear training result function

--- GestureRecognition1.03/wear/src/main/java/com/example/wangqianyi/gesturerecognition103/MainApp.java
+++ GestureRecognition1.03/wear/src/main/java/com/example/wangqianyi/gesturerecognition103/MainApp.java
@@ -2,16 +2,26 @@ package com.example.wangqianyi.gesturerecognition103;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
+
+import java.util.Locale;
 
 public class MainApp extends Activity {
 
     ToggleButton trainingToggle;
+    Button clearButton;
     public static final String BROADCAST_ACTION = "net.qianyiw.broadcast.mainapp";
     Intent intent;
+    TextToSpeech t1;
+    SharedPreferences.Editor training_editor;
+    public static final String TRAINING_FILE = "TrainingFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,16 @@ public class MainApp extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         intent = new Intent(BROADCAST_ACTION);
         trainingToggle = (ToggleButton)findViewById(R.id.togglebutton);
+        clearButton = (Button)findViewById(R.id.clearButton);
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.US);
+                }
+            }
+        });
+        training_editor = getSharedPreferences(TRAINING_FILE, MODE_PRIVATE).edit();
     }
 
     @Override
@@ -34,10 +54,19 @@ public class MainApp extends Activity {
                 if (isChecked) {
                     intent.putExtra("training_toggle", true);
                     sendBroadcast(intent);
+                    t1.speak("please do single inside to train", TextToSpeech.QUEUE_FLUSH, null);
                 } else {
                     intent.putExtra("training_toggle", false);
                     sendBroadcast(intent);
                 }
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                training_editor.clear().commit();
+                t1.speak("you have cleared all training result", TextToSpeech.QUEUE_FLUSH, null);
             }
         });
     }
